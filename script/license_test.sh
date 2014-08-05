14782598e2a76c6f077894099ad4a70792daee69
Matt Larson <coslars@hotmail.com>
2014-08-05 13:33:35 -0600

    More triggers -9

--- script/license_test.sh
+++ script/license_test.sh
@@ -2,6 +2,8 @@
 
 echo "--- About to run license_finder. ---"
 echo "Current Directory: " $PWD
+license_finder
+echo "Current is now: " $PWD
 cd ..
 license_finder
 echo "--- license_finder finished. ---"
