bc0542b1bcbfad79977acb78ae85d5cfb138beaa
Matt Larson <coslars@hotmail.com>
2014-08-05 14:37:05 -0600

    More triggers -15

--- script/license_test.sh
+++ script/license_test.sh
@@ -2,8 +2,12 @@
 
 echo "--- About to run license_finder. ---"
 echo "Current Directory: " $PWD
-#gem install rake
-#gem install license_finder
+gem install rake
+gem install i18n
+gem install license_finder
+license_finder
+echo "Running again with full bundle install: "
+bundle install
 license_finder
 echo "Current Directory: " $PWD
 echo "--- license_finder finished. ---"
