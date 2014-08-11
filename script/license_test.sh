5b7b543e93918469b91a3fb0b7a985aeddac8ada
Matt Larson <coslars@hotmail.com>
2014-08-11 09:08:29 -0600

    More Travis Changes2

--- script/license_test.sh
+++ script/license_test.sh
@@ -2,7 +2,6 @@
 
 echo "--- About to run license_finder. ---"
 echo "Current Directory: " $PWD
-bundle install
 license_finder
 echo "Current Directory: " $PWD
 echo "--- license_finder finished. ---"
