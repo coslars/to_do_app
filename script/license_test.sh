a741c0a518e55319be2fecce6311d88ca70b717e
Matt Larson <coslars@hotmail.com>
2014-08-11 10:47:39 -0600

    More Travis Changes7

--- script/license_test.sh
+++
@@ -1,7 +0,0 @@
- #! /bin/bash
-
-echo "--- About to run license_finder. ---"
-echo "Current Directory: " $PWD
-license_finder
-echo "Current Directory: " $PWD
-echo "--- license_finder finished. ---"
