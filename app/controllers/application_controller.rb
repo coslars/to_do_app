57875e48126b00dfd80763098a554e22647a8b52
Matt Larson <coslars@hotmail.com>
2014-08-12 15:01:35 -0600

    Trying to get Code Climate to Alert me.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  include ApplicationHelper
 
   # Lock down the application run this filter on everything
   #  If there is a controller that needs to allow access to non-authenticated
   #  users then add skip_before_filter :validate_login
   #  or skip_before_filter :validate_login, :except => [:method1, :method2]
   before_filter :validate_login
+
+  # I'm going to make this line super long to see if it's caught by Code Climate, I'm going to make this line super long to see if it's caught by Code Climate, I'm going to make this line super long to see if it's caught by Code Climate, I'm going to make this line super long to see if it's caught by Code Climate,
 
 end
